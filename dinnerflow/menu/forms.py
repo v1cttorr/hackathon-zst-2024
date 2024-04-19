be1228ed8aaba4cc52a2eec54a43c4451967d85a
VP859 <mierzwa.jan.6@gmail.com>
2024-04-19 12:45:07 +0200

    auto delete after time

--- dinnerflow/menu/forms.py
+++ dinnerflow/menu/forms.py
@@ -5,22 +5,23 @@ class Meal_add_form(forms.Form):
     meal_name = forms.CharField(max_length=100)
     meal_description = forms.CharField(widget=forms.Textarea)
     meal_price = forms.DecimalField(max_digits=5, decimal_places=2)
-    meal_image = forms.ImageField()
     allergens = forms.MultipleChoiceField(choices=[('gluten', 'Gluten'), ('jaja', 'Jaja'), ('orzechy', 'Orzechy'), ('nabiał', 'Nabiał'), ('sezam', 'Sezam')], widget=forms.CheckboxSelectMultiple)
+    date = forms.DateField()
 
     class Meta:
-        fields = ['meal_name', 'meal_description', 'meal_price', 'meal_image', 'allergens']
+        fields = ['meal_name', 'meal_description', 'meal_price', 'allergens', 'date']
 
     def save(self):
         meal = Meal.objects.create(
             meal_name=self.cleaned_data['meal_name'],
             meal_description=self.cleaned_data['meal_description'],
             meal_price=self.cleaned_data['meal_price'],
-            meal_image=self.cleaned_data['meal_image'],
-            allergens=self.cleaned_data['allergens']
+            allergens=self.cleaned_data['allergens'],
+            date=self.cleaned_data['date'],
         )
         return meal
+    
 class Meal_edit_form(forms.ModelForm):
     class Meta:
         model = Meal
-        fields = ['meal_name', 'meal_description', 'meal_price', 'meal_image', 'allergens']
+        fields = ['meal_name', 'meal_description', 'meal_price', 'allergens', 'date']
