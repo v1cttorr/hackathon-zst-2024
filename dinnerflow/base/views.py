7d349805866369d32ec66f9a1132c24457644f27
v1cttorr <murdziawiktor@gmail.com>
2024-04-19 10:23:06 +0200

    client model

--- dinnerflow/base/views.py
+++ dinnerflow/base/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from accounts.models import Client
 
 # Create your views here.
 def home(request):
+    clients = Client.objects.all()
+    for client in clients:
+        client.checkDate()
     return render(request, 'home.html')
+
+def scan(request):
+    return render(request, 'base/scan.html')
